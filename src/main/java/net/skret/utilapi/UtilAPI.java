95e30aceedef038a25bee85aa675d7cf0b0e1d72
MCreeper12731 <vitko44@gmail.com>
2022-12-05 20:28:52 +0100

    Initial commit

---
+++ src/main/java/net/skret/utilapi/UtilAPI.java
@@ -0,0 +1,17 @@
+package net.skret.utilapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UtilAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
